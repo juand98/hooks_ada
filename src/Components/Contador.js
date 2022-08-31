abdf6de3ffbc83400a4aefebfcfd619b9d3f8563
juand98 <juanduartet.98@gmail.com>
2022-08-31 16:11:00 -0500

    forma 2

--- src/Components/Contador.js
+++ src/Components/Contador.js
@@ -3,11 +3,27 @@ import React, { useState } from "react";
 function Contador() {
   const [cuenta, setCuenta] = useState(100);
 
+  const aumentar = () => {
+    setCuenta(cuenta + 1);
+  };
+  const disminuir = () => {
+    setCuenta(cuenta - 1);
+  };
+  const reset = () => {
+    setCuenta(100);
+  };
+
   return (
     <div>
       <center>
+        {/* Forma 1 */}
         <h1>Cuenta:{cuenta}</h1>
-        <button
+        <button onClick={aumentar}>Aumentar</button>
+        <button onClick={disminuir}>Disminuir</button>
+        <button onClick={reset}>Reset</button>
+
+        {/* Forma 2 */}
+        {/* <button
           onClick={() => {
             setCuenta(cuenta + 1);
           }}
@@ -27,7 +43,7 @@ function Contador() {
           }}
         >
           Reset
-        </button>
+        </button> */}
       </center>
     </div>
   );
